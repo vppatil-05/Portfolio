f03d0459e2032f46fbc978260e6ecdd2cf6c70c5
vppatil-05 <pophalevaishnavi01@gmail.com>
2026-01-11 15:09:30 +0530

    First Page

---
+++ main.js
@@ -0,0 +1,8 @@
+const typed=new Typed(".text",{
+    strings:["a Web Designer.","an App Developer.","a Frontend Developer."],
+    typeSpeed:60,
+    backSpeed:30,
+    backDelay:1200,
+    loop:true
+
+});
